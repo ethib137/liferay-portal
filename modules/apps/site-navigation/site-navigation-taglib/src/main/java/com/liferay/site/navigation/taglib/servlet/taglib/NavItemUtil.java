3140d181936720ca3ca7392505da54d72b5795bb
Evan Thibodeau <evan.thibodeau@liferay.com>
2022-09-15 14:02:38 +0400

    LPS-140746 - Copy _getMenuNavItems from NavigationMenuTag to NavItemUtil.

--- modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavItemUtil.java
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavItemUtil.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.theme.NavItem;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -141,6 +142,47 @@ public class NavItemUtil {
 		}
 
 		return navItems;
+	}
+
+	public List<NavItem> getMenuNavItems(
+			HttpServletRequest httpServletRequest, List<NavItem> branchNavItems)
+		throws Exception {
+
+		if (_rootItemType.equals("absolute")) {
+			if (_rootItemLevel == 0) {
+				return NavItemUtil.getChildNavItems(
+					httpServletRequest, _siteNavigationMenuId, 0);
+			}
+			else if (branchNavItems.size() >= _rootItemLevel) {
+				NavItem rootNavItem = branchNavItems.get(_rootItemLevel - 1);
+
+				return rootNavItem.getChildren();
+			}
+		}
+		else if (_rootItemType.equals("relative") && (_rootItemLevel >= 0) &&
+				 (_rootItemLevel < (branchNavItems.size() + 1))) {
+
+			int absoluteLevel = branchNavItems.size() - 1 - _rootItemLevel;
+
+			if (absoluteLevel == -1) {
+				return NavItemUtil.getChildNavItems(
+					httpServletRequest, _siteNavigationMenuId, 0);
+			}
+			else if ((absoluteLevel >= 0) &&
+					 (absoluteLevel < branchNavItems.size())) {
+
+				NavItem rootNavItem = branchNavItems.get(absoluteLevel);
+
+				return rootNavItem.getChildren();
+			}
+		}
+		else if (_rootItemType.equals("select")) {
+			return NavItemUtil.getChildNavItems(
+				httpServletRequest, _siteNavigationMenuId,
+				GetterUtil.getLong(_rootItemId));
+		}
+
+		return new ArrayList<>();
 	}
 
 	public static List<NavItem> getNavItems(
