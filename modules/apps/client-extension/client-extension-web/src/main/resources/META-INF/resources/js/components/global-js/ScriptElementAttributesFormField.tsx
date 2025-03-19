f7bd1ebb2ddc1c52b896853cb572753de8ffb422
Evan Thibodeau <evan.thibodeau@liferay.com>
2025-03-19 16:26:06 +0400

    LPD-46909 Update layout to match new designs and clarify that if a attribute exists, the name is required.

--- modules/apps/client-extension/client-extension-web/src/main/resources/META-INF/resources/js/components/global-js/ScriptElementAttributesFormField.tsx
+++ modules/apps/client-extension/client-extension-web/src/main/resources/META-INF/resources/js/components/global-js/ScriptElementAttributesFormField.tsx
@@ -3,16 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton from '@clayui/button';
+
 // @ts-nocheck
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
+import {disableFormSubmitButton} from '../../clientExtensionUtil';
 import AttributeFields, {TYPE_BOOLEAN, TYPE_STRING} from './AttributeFields';
+
+type Attribute = {
+	id: string;
+	name: string;
+	type: string;
+	value: boolean | string;
+};
+
+type PartialAttribute = {
+	name?: string;
+	type?: string;
+	value?: string | boolean;
+};
 
 const emptyRow = () => ({id: uuidv4(), name: '', type: TYPE_STRING, value: ''});
 
-const toJSONObjectString = (attributes) => {
+const toJSONObjectString = (attributes: Attribute[]) => {
 	const validAttributes = attributes.filter(
 		(attribute) => attribute.name.trim().length
 	);
@@ -21,16 +37,12 @@ const toJSONObjectString = (attributes) => {
 		return '';
 	}
 
-	const attributesObject = {};
+	const attributesObject: {
+		[key: string]: boolean | string;
+	} = {};
 
 	validAttributes.forEach((attribute) => {
-		let value = attribute.value;
-
-		if (attribute.type === 'Boolean') {
-			value = JSON.parse(value);
-		}
-
-		attributesObject[attribute.name] = value;
+		attributesObject[attribute.name] = attribute.value;
 	});
 
 	return JSON.stringify(attributesObject);
@@ -58,31 +70,66 @@ interface IProps {
 	scriptElementAttributesJSON?: string;
 }
 
+type Errors = {
+	[key: number]: any;
+};
+
 export default function ScriptElementAttributesFormField({
 	disabled,
 	portletNamespace,
 	scriptElementAttributesJSON: initialAttributes,
 }: IProps) {
-	const [attributes, setAttributes] = useState(() =>
-		initialAttributes ? parseAttributes(initialAttributes) : [emptyRow()]
+	const [attributes, setAttributes] = useState<Attribute[]>(() =>
+		initialAttributes ? parseAttributes(initialAttributes) : []
 	);
+	const [errors, setErrors] = useState<Errors>({});
 
-	const handleAddClick = (index) => {
-		setAttributes(attributes.toSpliced(index + 1, 0, emptyRow()));
+	const numberOfErrors = Object.keys(errors).length;
+
+	useEffect(() => {
+		disableFormSubmitButton(numberOfErrors > 0, portletNamespace);
+	}, [numberOfErrors, portletNamespace]);
+
+	const handleAddClick = () => {
+		setAttributes(attributes.toSpliced(attributes.length, 0, emptyRow()));
 	};
 
-	const handleAttributeChange = (index, updatedValue) => {
+	const handleAttributeChange = (
+		index: number,
+		updatedValue: PartialAttribute
+	) => {
 		setAttributes((prevList) =>
 			prevList.with(index, {...prevList[index], ...updatedValue})
 		);
 	};
 
-	const handleRemoveClick = (index) => {
+	const handleErrorChange = (index: number, error: boolean) => {
+		if (error) {
+			setErrors((prevErrors) => ({...prevErrors, [index]: true}));
+		}
+		else {
+			setErrors((prevErrors) => {
+				const newErrors = {...prevErrors};
+
+				delete newErrors[index];
+
+				return newErrors;
+			});
+		}
+	};
+
+	const handleRemoveClick = (index: number) => {
 		setAttributes(attributes.toSpliced(index, 1));
 	};
 
 	return (
-		<>
+		<fieldset role="group">
+			<legend>
+				<span className="mb-0 sheet-tertiary-title">
+					{Liferay.Language.get('attributes')}
+				</span>
+			</legend>
+
 			<input
 				disabled={disabled}
 				name={`${portletNamespace}scriptElementAttributesJSON`}
@@ -96,14 +143,18 @@ export default function ScriptElementAttributesFormField({
 					index={index}
 					key={attribute.id}
 					name={attribute.name}
-					onAddClick={handleAddClick}
 					onAttributeChange={handleAttributeChange}
+					onErrorChange={handleErrorChange}
 					onRemoveClick={handleRemoveClick}
 					portletNamespace={portletNamespace}
 					type={attribute.type}
 					value={attribute.value}
 				/>
 			))}
-		</>
+
+			<ClayButton displayType="secondary" onClick={handleAddClick}>
+				{Liferay.Language.get('add-attribute-group')}
+			</ClayButton>
+		</fieldset>
 	);
 }
